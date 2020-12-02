4269f8bea231c2386eb5c53091b19f8ba9b3778a
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-12-01 23:36:22 -0800

    scrollreveal

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 // scrollreveal animation
 
 window.sr = ScrollReveal();
+
 sr.reveal('.col-for-text-heading',{
   duration:2000,
   origin:'left',
-  distance:"200px"});
+  viewFactor:0.2,
+  distance:"200px"}
+  );
 
 
+  // window.sr = ScrollReveal();
+  // sr.reveal('.col-for-img-heading',{
+  //   duration:2000,
+  //   origin:'right',
+  //   viewFactor:0.2,
+  //   distance:"200px"
+  // });
 
-  sr.reveal('.col-for-img-heading',{
-    duration:2000,
-    origin:'right',
-    distance:"200px"});
 
+  //   window.sr = ScrollReveal();
+  //   sr.reveal('.heading-journey, .paragraph-for-journey',{
+  //     duration:2000,
+  //     origin:'bottom',
+  //     viewFactor:0.2,
+  //     distance:"200px"
+  //   });
 
   
+
+// sr.reveal('.section-2',{
+//         duration:2000,
+//         origin:'right',
+//         viewFactor:0.2,
+//         distance:"200px"}
+//         );
