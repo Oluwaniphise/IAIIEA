d222552a4475902f02d5c1bdd008c81def00831a
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-12-02 04:22:31 -0800

    added footer

--- app.js
+++ app.js
@@ -6,26 +6,25 @@ sr.reveal('.col-for-text-heading',{
   duration:2000,
   origin:'left',
   viewFactor:0.2,
-  distance:"200px"}
-  );
+  distance:"200px"
+});
 
 
-  // window.sr = ScrollReveal();
-  // sr.reveal('.col-for-img-heading',{
-  //   duration:2000,
-  //   origin:'right',
-  //   viewFactor:0.2,
-  //   distance:"200px"
-  // });
+sr.reveal('.col-for-img-heading',{
+    duration:2000,
+    origin:'right',
+    viewFactor:0.2,
+    distance:"200px"
+  });
 
 
-  //   window.sr = ScrollReveal();
-  //   sr.reveal('.heading-journey, .paragraph-for-journey',{
-  //     duration:2000,
-  //     origin:'bottom',
-  //     viewFactor:0.2,
-  //     distance:"200px"
-  //   });
+    // window.sr = ScrollReveal();
+    // sr.reveal('.heading-journey, .paragraph-for-journey',{
+    //   duration:2000,
+    //   origin:'bottom',
+    //   viewFactor:0.2,
+    //   distance:"200px"
+    // });
 
   
 
