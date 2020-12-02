57eb1a46d06b570521c04c7472991e049d4a1467
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-12-01 23:12:14 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// scrollreveal animation
+
+window.sr = ScrollReveal();
+sr.reveal('.col-for-text-heading',{
+  duration:2000,
+  origin:'left',
+  distance:"200px"});
+
+
+
+  sr.reveal('.col-for-img-heading',{
+    duration:2000,
+    origin:'right',
+    distance:"200px"});
+
+
+  
