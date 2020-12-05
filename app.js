250d8539ba860da6e02b8c15ceb590f7dd94c514
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-12-05 12:38:51 -0800

    fixing seminar and conference page

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
+$("#countdown")
+  .countdown("2020/12/31", function(event) {
+    $(this).text(
+      event.strftime('%D  %H:%M:%S')
+    );
+  });
 
 
-
-// scrollreveal animation
-
-window.sr = ScrollReveal();
-
-sr.reveal('.col-for-text-heading',{
-  duration:2000,
-  origin:'left',
-  viewFactor:0.2,
-  distance:"200px"
-});
-
-
-// sr.reveal('.col-for-img-heading',{
-//     duration:2000,
-//     origin:'right',
-//     viewFactor:0.2,
-//     distance:"200px"
-//   });
-
-
-    // window.sr = ScrollReveal();
-    // sr.reveal('.heading-journey, .paragraph-for-journey',{
-    //   duration:2000,
-    //   origin:'bottom',
-    //   viewFactor:0.2,
-    //   distance:"200px"
-    // });
-
-  
-
-// sr.reveal('.section-2',{
-//         duration:2000,
-//         origin:'right',
-//         viewFactor:0.2,
-//         distance:"200px"}
-//         );
-
-
-// navbar scroll effect for conference, seminar, workshop
-
-
-
+  window.sr = ScrollReveal();
+  sr.reveal('.conference-heading, .conference-time, .conference-timer, .conference-theme', {
+      duration: 2000,
+      origin: 'top',
+      distance: "200px",
+      viewFactor:0.2
+  });
 
