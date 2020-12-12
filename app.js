e8746c77b1863e98f57d7d4f39cbf0cab8c72556
Enoch Oduyale <oduyaleenoch@gmail.com>
2020-12-12 08:51:25 -0800

    finished home page

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+window.addEventListener('scroll', function(){
+    var navbar = document.querySelector('.navbar-conference');
+    navbar.classList.toggle('sticky', window.scrollY > 0);
+   
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 $("#countdown")
   .countdown("2020/12/31", function(event) {
     $(this).text(
-      event.strftime('%D  %H:%M:%S')
+      event.strftime('%D days  %H:%M:%S')
     );
   });
 
-  AOS.init(
-    
-  );
+  
+
+
+
 
 
   
