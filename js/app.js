f4d4358a446442f2a98ac748f1f6979d5d68edd1
Enoch Oduyale <oduyaleenoch@gmail.com>
2021-01-01 09:14:50 -0800

    added current page link bg

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 
 
 
-
+// for programmes blue bakcground
 window.addEventListener('scroll', function(){
     var navbar = document.querySelector('.navbar-conference');
     navbar.classList.toggle('sticky', window.scrollY > 0);
    
   });
 
+// 
+$(document).on('click', '.navbar-nav .navbar-item', function(){
+  $(this).addClass('active').siblings().removeClass('active');
+})
 
+
+
+
+
+
+
+// for conference timer
 
 $("#conference-timer")
   .countdown("2021/08/25", function(event) {
